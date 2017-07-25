37eec8cfd3eafdc429169b9866079388354ee2be
Florian Le Roux <florian.le-roux02@orange.fr>
2017-07-25 13:26:54 +0200

    add tag latest in build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ cd `dirname "${BASH_SOURCE[0]}"`
 image="node-file-manager"
 version=`git describe --tags --exact-match 2> /dev/null || git rev-parse --short HEAD`
 docker build . -t $image:$version
+docker tag $image:{$version,latest}
